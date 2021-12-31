7cbc2bdd22a6db37b89a533ad9eddea354f805ca
pham-k <pham.k@protonmail.com>
2021-12-31 17:05:46 +0700

    Add solution to tuan-00

--- tuan-00/20880040/SOURCE/Program.cs
+++ tuan-00/20880040/SOURCE/Program.cs
@@ -6,25 +6,25 @@ namespace SOURCE
     {
         static void Main(string[] args)
         {
-            // AdjacencyMatrix AM = new AdjacencyMatrix();
-            // AM.ReadData("./adjacency-matrix-2.txt");
-            // AM.PrintGraph();
-            // if (AM.IsSymmetric()) {
-            //     Console.WriteLine("Ma tran doi xung");
-            // } else {
-            //     Console.WriteLine("Ma tran khong doi xung");
-            // }
-            // var al = AM.ToAdjacencyList();
-            // al.WriteData("./adjacency-list.txt");
+            AdjacencyMatrix AM = new AdjacencyMatrix();
+            AM.ReadData("./adjacency-matrix-2.txt");
+            AM.PrintGraph();
+            if (AM.IsSymmetric()) {
+                Console.WriteLine("Ma tran doi xung");
+            } else {
+                Console.WriteLine("Ma tran khong doi xung");
+            }
+            var al = AM.ToAdjacencyList();
+            al.WriteData("./adjacency-list.txt");
 
             AdjacencyList AL = new AdjacencyList();
             AL.ReadData("./adjacency-list-2.txt");
-            // AL.PrintGraph();
-            // if (AL.IsUndirected()) {
-            //     Console.WriteLine("Do thi vo huong");
-            // } else {
-            //     Console.WriteLine("Do thi co huong");
-            // }
+            AL.PrintGraph();
+            if (AL.IsUndirected()) {
+                Console.WriteLine("Do thi vo huong");
+            } else {
+                Console.WriteLine("Do thi co huong");
+            }
             var am = AL.ToAdjacencyMatrix();
             am.WriteData("./adjacency-matrix.txt");
         }
