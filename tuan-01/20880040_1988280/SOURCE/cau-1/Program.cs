55ab6c7cc2a53e0607518e931ad83820ecad88ed
pham-k <pham.k@protonmail.com>
2022-02-03 16:31:05 +0700

    Organize tuan-01 according to requirements

--- tuan-01/20880040_1988280/SOURCE/cau-1/Program.cs
+++ tuan-01/20880040_1988280/SOURCE/cau-1/Program.cs
@@ -7,7 +7,7 @@ namespace SOURCE
         static void Main(string[] args)
         {
             AdjacencyMatrix AM1 = new AdjacencyMatrix();
-            AM1.ReadData("./input-6.txt");
+            AM1.ReadData("./input-1.txt");
             var AL1 = AM1.ToAdjacencyList();
             // AM1.PrintGraph();
             // AL1.PrintGraph();
@@ -48,25 +48,6 @@ namespace SOURCE
             }
             // Cau 1h
             Console.WriteLine(AM1.GetGraphType());
-
-            // Cau 2
-            if (AM1.IsComplete() >= 0) {
-                Console.WriteLine($"Day la do thi day du K{AM1.IsComplete()}");
-            } else {
-                Console.WriteLine("Day khong phai la do thi day du");
-            }
-
-            if (AM1.IsRegular() >= 0) {
-                Console.WriteLine($"Day la do thi {AM1.IsRegular()}-chinh quy");
-            } else {
-                Console.WriteLine("Day khong phai la do thi chinh quy");
-            }
-            
-            if (AL1.IsCycle()) {
-                Console.WriteLine($"Day la do thi vong C{AL1.GetNumberOfVertices()}");
-            } else {
-                Console.WriteLine("Day khong phai do thi vong");
-            }
         }
     }
 }
---
+++ tuan-01/20880040_1988280/SOURCE/cau-2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SOURCE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdjacencyMatrix AM1 = new AdjacencyMatrix();
+            AM1.ReadData("./input-3.txt");
+            var AL1 = AM1.ToAdjacencyList();
+            // AM1.PrintGraph();
+            // AL1.PrintGraph();
+            
+            // Cau 2
+            // Cau 2a
+            if (AM1.IsComplete() >= 0) {
+                Console.WriteLine($"Day la do thi day du K{AM1.IsComplete()}");
+            } else {
+                Console.WriteLine("Day khong phai la do thi day du");
+            }
+            // Cau 2b
+            if (AM1.IsRegular() >= 0) {
+                Console.WriteLine($"Day la do thi {AM1.IsRegular()}-chinh quy");
+            } else {
+                Console.WriteLine("Day khong phai la do thi chinh quy");
+            }
+            // Cau 2c
+            if (AL1.IsCycle()) {
+                Console.WriteLine($"Day la do thi vong C{AL1.GetNumberOfVertices()}");
+            } else {
+                Console.WriteLine("Day khong phai do thi vong");
+            }
+        }
+    }
+}
